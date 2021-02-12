10dd22e230cefdb35e80d008af0830980431c959
khushilms-cs19 <67272210+khushilms-cs19@users.noreply.github.com>
2021-02-12 10:37:49 +0530

    Add files via upload

---
+++ Black Jack Game final/BlackJackMain.java
@@ -0,0 +1,8 @@
+public class BlackJackMain 
+{
+    public static int wincount=0;
+    public static void main(String args[])
+    {
+        new GameFrame();
+    }
+}
---
+++ Black Jack Game final/GameFrame.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+
+    GamePanel board;
+    GameFrame()
+    {
+        board = new GamePanel();
+        this.setContentPane(board);
+        // this.add(new GamePanel());
+        this.setTitle("Black Jack ");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLayout(null);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+    // static public void restart()
+    // {
+    //     board=new GamePanel();
+    //     this.setContentPane(board);
+    //     // this.add(new GamePanel());
+    //     this.setTitle("Black Jack ");
+    //     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     this.setResizable(false);
+    //     this.pack();
+    //     this.setLayout(null);d
+    //     this.setVisible(true);
+    //     this.setLocationRelativeTo(null);
+    // }
+}
